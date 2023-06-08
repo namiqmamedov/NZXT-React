1b135dd51360edc0c795823da9995e4896dec8fe
namiqmamedov <namigmm@code.edu.az>
2023-06-08 15:20:55 +0400

    Created home page

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -1,10 +1,15 @@
 import React from 'react'
 import Header from '../Header/Header'
+import Routers from '../../routes/Routers'
 
 const Layout = () => {
   return (
     <>
         <Header/>
+        <>
+            <Routers/>
+        </>
+         {/* <Footer/> */}
     </>
   )
 }
