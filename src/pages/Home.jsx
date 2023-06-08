f5874561e9ca79914bd025432023822626395365
namiqmamedov <namigmm@code.edu.az>
2023-06-08 15:52:17 +0400

    Updated home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,28 @@
 import React from 'react'
+import HeroBackground from '../assets/images/home/heroBackground.avif'
+import HeroCenter from '../assets/images/home/heroCenter.avif'
+import twologo from '../assets/images/home/two-logo.png'
+import '../styles/home.css'
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <div className='home__index'>
+        <img className='background-img' src={HeroBackground} alt="Home Background Image" />
+        <div className="container">
+            <div className="row">
+                <div className="home__content">
+                    <div className="home__text col-lg-6">
+                        <img src={twologo} alt="" />
+                        <h1>Ready Player: Two</h1>
+                        <p>Game, stream and create.</p>
+                    </div>
+                    <div className="home__thumbnail col-lg-6">
+                    <img className='home-img' src={HeroCenter} alt="Image" />
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
   )
 }
 
