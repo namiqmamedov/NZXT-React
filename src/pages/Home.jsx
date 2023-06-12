b6dba1d096103bb8429d784422f235d1b637cf66
namiqmamedov <namigmm@code.edu.az>
2023-06-13 02:55:51 +0400

    Updated main section

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import HeroBackground from "../assets/images/home/heroBackground.avif";
+import BottomThumbnail from "../assets/images/home/bottomThumbnail.avif";
 import HeroCenter from "../assets/images/home/heroCenter.avif";
 import twologo from "../assets/images/home/two-logo.png";
 import Button from "@mui/material/Button";
@@ -8,20 +9,26 @@ import Radio from "@mui/material/Radio";
 import RadioGroup from "@mui/material/RadioGroup";
 import FormControlLabel from "@mui/material/FormControlLabel";
 import FormControl from "@mui/material/FormControl";
-import FormLabel from "@mui/material/FormLabel";
 import card01 from "../assets/images/product/card01.png";
 import card02 from "../assets/images/product/card02.png";
 import card03 from "../assets/images/product/card03.avif";
 import card04 from "../assets/images/product/card04.avif";
 import card05 from "../assets/images/product/card05.avif";
 import card06 from "../assets/images/product/card06.avif";
+import card07 from "../assets/images/product/card07.avif";
+import card08 from "../assets/images/product/card08.avif";
+import brand01 from '../assets/images/home/brand01.png'
+import brand02 from '../assets/images/home/brand02.png'
+import brand03 from '../assets/images/home/brand03.png'
 import thumbnail01 from "../assets/images/product/thumbnail01.png";
 import thumbnailLogo from "../assets/images/product/thumbnailLogo.png";
+import affirmLogo from '../assets/images/home/affirmLogo.png'
 import promoBanner from "../assets/images/home/promo-banner.png";
 import promoImg01 from "../assets/images/home/promo-img01.png";
 import promoLogo01 from "../assets/images/home/promo-logo01.png";
 import primeLogo01 from "../assets/images/home/prime-logo01.png";
 import mainBackground from "../assets/images/home/mainBackground.avif";
+import mainBackground2 from "../assets/images/home/mainBackground2.png";
 import newsCard01 from "../assets/images/home/newsCard01.png";
 import newsCard02 from "../assets/images/home/newsCard02.png";
 import newsCard02Item from "../assets/images/home/newsCard02Item.png";
@@ -33,10 +40,28 @@ import newsCard06 from "../assets/images/home/newsCard06.png";
 import newsCard07 from "../assets/images/home/newsCard07.png";
 import { MdKeyboardArrowRight } from "react-icons/md";
 
+const brandData = [
+  {
+    id: '01',
+    image: brand01,
+    desc: '“Offering real-world performance data is a great idea, and makes the process of ordering a PC much less abstract for newcomers.”' 
+  },
+  {
+    id: '02',
+    image: brand02,
+    desc: '“There’s something lovely about having a PC that comes with a set of guaranteed predicted frames-per-second targets for the games I planned.”' 
+  },
+  {
+    id: '03',
+    image: brand03,
+    desc: '“This is certainly a very neat idea, and the company is putting its money where its mouth is regarding the frame-rate estimations.”' 
+  }
+]
+
 const Home = () => {
   return (
     <>
-      <div className="home__index">
+      <section className="home__index">
         <img
           className="background-img"
           src={HeroBackground}
@@ -57,9 +82,9 @@ const Home = () => {
             </div>
           </div>
         </div>
-      </div>
-      <div className="player-two-card">
-        <div className="container">
+      </section>
+      <section className="player-two-card">
+      <div className="container">
           <div className="row">
             {/* <p>A PC BUILT TO GAME</p>
             <p>Player: Two brings powerful, built-to-last performance to anything from competitive FPS to breathtaking RPGs.</p> */}
@@ -126,9 +151,9 @@ const Home = () => {
               </div>
             </div>
           </div>
-        </div>
       </div>
-      <div className="promo__banner">
+      </section>
+      <section className="promo__banner">
         <div className="container">
           <div className="row">
             <div className="image-block">
@@ -157,8 +182,8 @@ const Home = () => {
             </div>
           </div>
         </div>
-      </div>
-      <div className="main-wrapper">
+      </section>
+      <section className="main-wrapper">
         <div className="wrapper-content">
           <div className="wrapper-main">
             <img src={primeLogo01} alt="" />
@@ -175,8 +200,8 @@ const Home = () => {
           </div>
         </div>
         <img className="background-img" src={mainBackground} alt="" />
-      </div>
-      <div className="newest-releases">
+      </section>
+      <section className="newest-releases">
         <div className="container">
           <div className="row">
             <h2 className="title">Our Newest Releases</h2>
@@ -323,9 +348,8 @@ const Home = () => {
             </div>
           </div>
         </div>
-      </div>
-
-      <div className="shop-bestseller">
+      </section>
+      <section className="shop-bestseller">
         <div className="container">
           <div className="row">
             <h1 className="title">Shop NZXT Bestsellers</h1>
@@ -338,6 +362,9 @@ const Home = () => {
               <div className="shop-card">
                 <div className="card-thumbnail">
                   <img src={card01} alt="Shop Card" />
+                  <div className="quick-shop">
+                    <p>Quick Shop</p>
+                  </div>
                 </div>
                 <div className="card-body">
                   <h5 className="card-title">Player: Two</h5>
@@ -371,7 +398,7 @@ const Home = () => {
                 <div className="card-thumbnail">
                   <img src={card01} alt="Shop Card" />
                 </div>
-                <h5 className="card-title">Player: Two</h5>
+                <h5 className="card-title">Player: Two Prime</h5>
                 <p className="card-subtitle">H5 Elite RTX 4070 Gaming PC</p>
                 <div className="promo-active">
                   <svg
@@ -416,9 +443,232 @@ const Home = () => {
                   <span className="badge-main">
                     <p>$100 Off</p>
                   </span>
-                  <img src={card01} alt="Shop Card" />
+                  <img src={card03} alt="Shop Card" />
                 </div>
-                <h5 className="card-title">Player: Two</h5>
+                <h5 className="card-title">Player: Three</h5>
+                <p className="card-subtitle">H5 Elite RTX 4070 Gaming PC</p>
+                <div className="promo-active">
+                  <svg
+                    width="1em"
+                    height="1em"
+                    fill="currentColor"
+                    viewBox="0 0 24 24"
+                    class="hidden md:block w-4 h-4 mr-1"
+                  >
+                    <path d="M1.88 17.742l.12.008h20l.12-.008c.84-.11.84-1.375 0-1.484L22 16.25A2.25 2.25 0 0119.75 14V9a7.75 7.75 0 00-15.5 0v5a2.25 2.25 0 01-2.096 2.245L2 16.25c-.958 0-.998 1.378-.12 1.492zM12 2.75A6.25 6.25 0 0118.25 9v5l.005.2a3.73 3.73 0 00.675 1.954l.071.096H4.998l.072-.096c.428-.61.68-1.352.68-2.154V9A6.25 6.25 0 0112 2.75zm1.081 17.874a.75.75 0 111.298.752 2.75 2.75 0 01-4.758 0 .75.75 0 011.24-.837l.058.085a1.25 1.25 0 002.081.123l.081-.123z"></path>
+                  </svg>
+                  See Promotion
+                </div>
+                <div className="card-bottom">
+                  <div className="price-index">
+                    <p className="old-price">$2,499.00</p>
+                    <p className="card-price">$1,599.00</p>
+                  </div>
+                  <div className="color-options">
+                    <FormControl>
+                      <RadioGroup
+                        aria-labelledby="demo-radio-buttons-group-label"
+                        defaultValue="1"
+                        name="radio-buttons-group"
+                      >
+                        <FormControlLabel
+                          className="color-radio"
+                          value="1"
+                          control={<Radio />}
+                        />
+                        <FormControlLabel
+                          className="color-radio"
+                          value="2"
+                          control={<Radio />}
+                        />
+                      </RadioGroup>
+                    </FormControl>
+                  </div>
+                </div>
+              </div>
+              <div className="shop-card">
+                <div className="card-thumbnail">
+                  <span className="badge-main">
+                    <p>$100 Off</p>
+                  </span>
+                  <img src={card04} alt="Shop Card" />
+                </div>
+                <h5 className="card-title">Player: Three Prime</h5>
+                <p className="card-subtitle">H5 Elite RTX 4070 Gaming PC</p>
+                <div className="promo-active">
+                  <svg
+                    width="1em"
+                    height="1em"
+                    fill="currentColor"
+                    viewBox="0 0 24 24"
+                    class="hidden md:block w-4 h-4 mr-1"
+                  >
+                    <path d="M1.88 17.742l.12.008h20l.12-.008c.84-.11.84-1.375 0-1.484L22 16.25A2.25 2.25 0 0119.75 14V9a7.75 7.75 0 00-15.5 0v5a2.25 2.25 0 01-2.096 2.245L2 16.25c-.958 0-.998 1.378-.12 1.492zM12 2.75A6.25 6.25 0 0118.25 9v5l.005.2a3.73 3.73 0 00.675 1.954l.071.096H4.998l.072-.096c.428-.61.68-1.352.68-2.154V9A6.25 6.25 0 0112 2.75zm1.081 17.874a.75.75 0 111.298.752 2.75 2.75 0 01-4.758 0 .75.75 0 011.24-.837l.058.085a1.25 1.25 0 002.081.123l.081-.123z"></path>
+                  </svg>
+                  See Promotion
+                </div>
+                <div className="card-bottom">
+                  <div className="price-index">
+                    <p className="old-price">$2,499.00</p>
+                    <p className="card-price">$1,599.00</p>
+                  </div>
+                  <div className="color-options">
+                    <FormControl>
+                      <RadioGroup
+                        aria-labelledby="demo-radio-buttons-group-label"
+                        defaultValue="1"
+                        name="radio-buttons-group"
+                      >
+                        <FormControlLabel
+                          className="color-radio"
+                          value="1"
+                          control={<Radio />}
+                        />
+                        <FormControlLabel
+                          className="color-radio"
+                          value="2"
+                          control={<Radio />}
+                        />
+                      </RadioGroup>
+                    </FormControl>
+                  </div>
+                </div>
+              </div>
+              <div className="shop-card">
+                <div className="card-thumbnail">
+                  <img src={card05} alt="Shop Card" />
+                  <div className="quick-shop">
+                    <p>Quick Shop</p>
+                  </div>
+                </div>
+                <div className="card-body">
+                  <h5 className="card-title">Player: Two</h5>
+                  <p className="card-subtitle">H5 Elite RTX 4070 Gaming PC</p>
+                  <div className="card-bottom">
+                    <p className="card-price">$1,599.00</p>
+                    <div className="color-options">
+                      <FormControl>
+                        <RadioGroup
+                          aria-labelledby="demo-radio-buttons-group-label"
+                          defaultValue="1"
+                          name="radio-buttons-group"
+                        >
+                          <FormControlLabel
+                            className="color-radio"
+                            value="1"
+                            control={<Radio />}
+                          />
+                          <FormControlLabel
+                            className="color-radio"
+                            value="2"
+                            control={<Radio />}
+                          />
+                        </RadioGroup>
+                      </FormControl>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div className="shop-card">
+                <div className="card-thumbnail">
+                  <img src={card06} alt="Shop Card" />
+                </div>
+                <h5 className="card-title">Player: Two Prime</h5>
+                <p className="card-subtitle">H5 Elite RTX 4070 Gaming PC</p>
+                <div className="promo-active">
+                  <svg
+                    width="1em"
+                    height="1em"
+                    fill="currentColor"
+                    viewBox="0 0 24 24"
+                    class="hidden md:block w-4 h-4 mr-1"
+                  >
+                    <path d="M1.88 17.742l.12.008h20l.12-.008c.84-.11.84-1.375 0-1.484L22 16.25A2.25 2.25 0 0119.75 14V9a7.75 7.75 0 00-15.5 0v5a2.25 2.25 0 01-2.096 2.245L2 16.25c-.958 0-.998 1.378-.12 1.492zM12 2.75A6.25 6.25 0 0118.25 9v5l.005.2a3.73 3.73 0 00.675 1.954l.071.096H4.998l.072-.096c.428-.61.68-1.352.68-2.154V9A6.25 6.25 0 0112 2.75zm1.081 17.874a.75.75 0 111.298.752 2.75 2.75 0 01-4.758 0 .75.75 0 011.24-.837l.058.085a1.25 1.25 0 002.081.123l.081-.123z"></path>
+                  </svg>
+                  See Promotion
+                </div>
+                <div className="card-bottom">
+                  <div className="price-index">
+                    <p className="card-price">$1,599.00</p>
+                  </div>
+                  <div className="color-options">
+                    <FormControl>
+                      <RadioGroup
+                        aria-labelledby="demo-radio-buttons-group-label"
+                        defaultValue="1"
+                        name="radio-buttons-group"
+                      >
+                        <FormControlLabel
+                          className="color-radio"
+                          value="1"
+                          control={<Radio />}
+                        />
+                        <FormControlLabel
+                          className="color-radio"
+                          value="2"
+                          control={<Radio />}
+                        />
+                      </RadioGroup>
+                    </FormControl>
+                  </div>
+                </div>
+              </div>
+              <div className="shop-card">
+                <div className="card-thumbnail">
+                  <span className="badge-main">
+                    <p>$100 Off</p>
+                  </span>
+                  <img src={card07} alt="Shop Card" />
+                </div>
+                <h5 className="card-title">Player: Three</h5>
+                <p className="card-subtitle">H5 Elite RTX 4070 Gaming PC</p>
+                <div className="promo-active">
+                  <svg
+                    width="1em"
+                    height="1em"
+                    fill="currentColor"
+                    viewBox="0 0 24 24"
+                    class="hidden md:block w-4 h-4 mr-1"
+                  >
+                    <path d="M1.88 17.742l.12.008h20l.12-.008c.84-.11.84-1.375 0-1.484L22 16.25A2.25 2.25 0 0119.75 14V9a7.75 7.75 0 00-15.5 0v5a2.25 2.25 0 01-2.096 2.245L2 16.25c-.958 0-.998 1.378-.12 1.492zM12 2.75A6.25 6.25 0 0118.25 9v5l.005.2a3.73 3.73 0 00.675 1.954l.071.096H4.998l.072-.096c.428-.61.68-1.352.68-2.154V9A6.25 6.25 0 0112 2.75zm1.081 17.874a.75.75 0 111.298.752 2.75 2.75 0 01-4.758 0 .75.75 0 011.24-.837l.058.085a1.25 1.25 0 002.081.123l.081-.123z"></path>
+                  </svg>
+                  See Promotion
+                </div>
+                <div className="card-bottom">
+                  <div className="price-index">
+                    <p className="old-price">$2,499.00</p>
+                    <p className="card-price">$1,599.00</p>
+                  </div>
+                  <div className="color-options">
+                    <FormControl>
+                      <RadioGroup
+                        aria-labelledby="demo-radio-buttons-group-label"
+                        defaultValue="1"
+                        name="radio-buttons-group"
+                      >
+                        <FormControlLabel
+                          className="color-radio"
+                          value="1"
+                          control={<Radio />}
+                        />
+                        <FormControlLabel
+                          className="color-radio"
+                          value="2"
+                          control={<Radio />}
+                        />
+                      </RadioGroup>
+                    </FormControl>
+                  </div>
+                </div>
+              </div>
+              <div className="shop-card">
+                <div className="card-thumbnail">
+                  <span className="badge-main">
+                    <p>$100 Off</p>
+                  </span>
+                  <img src={card08} alt="Shop Card" />
+                </div>
+                <h5 className="card-title">Player: Three Prime</h5>
                 <p className="card-subtitle">H5 Elite RTX 4070 Gaming PC</p>
                 <div className="promo-active">
                   <svg
@@ -462,7 +712,46 @@ const Home = () => {
             </div>
           </div>
         </div>
-      </div>
+      </section>
+      <section className="main-content-grid">
+        <div className="container">
+          <div className="row">
+            <div className="main-content">
+              <img src={affirmLogo} alt="Affirm Logo" title="Affirm Logo" />
+              <h3>Pay Later with Affirm</h3>
+              <p>Buy now, pay over time. Quick and easy. No hidden fees. Select "Affirm monthly payments"
+              during checkout.</p>
+            </div>
+            <img className="background-img" src={mainBackground2} alt="Main Image" />
+          </div>
+        </div>
+      </section>
+      <section className="brand-content">
+        <div className="container">
+          <div className="row">
+            <h1 className="title">What Others Say</h1>
+            <div className="card-main">
+              {brandData.map((item,index) => (
+                <div className="card-item col-lg-4" key={index}>
+                    <div className="card-body">
+                      <img src={item.image} alt="Brand Image" title="Brand Image" />
+                      <div className="description">
+                        {item.desc}
+                      </div>
+                    </div>
+                </div>
+              ))}
+            </div>
+          </div>
+        </div>
+      </section>
+      <section className="bottom-thumbnail">
+        <div className="content text-center">
+          <p className="text-white">NZXT</p>
+          <h1>Prebuilt Power to Love</h1>
+        </div>
+        <img src={BottomThumbnail} alt="Thumbnail" title="Thumbnail" />
+      </section>
     </>
   );
 };
