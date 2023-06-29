5108069a1281c27fd88e2cb36d46f1f6b29b6ab0
namiqmamedov <namigmm@code.edu.az>
2023-06-29 15:02:37 +0400

    Updated created product detail page

---
+++ src/pages/ProductDetails.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import '../styles/product-detail.css'
+
+const ProductDetails = () => {
+  return (
+    <div>ProductDetails</div>
+  )
+}
+
+export default ProductDetails
