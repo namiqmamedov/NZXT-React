217f111831ec877f82fc2f4bfdc384b1d01639da
namiqmamedov <namigmm@code.edu.az>
2023-07-03 13:03:37 +0400

    Updated cart page

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -4,10 +4,15 @@ import Button from '@mui/material/Button';
 import img01 from  '../assets/images/thumbnail/prebuilts-primary-web.avif'
 import img02 from '../assets/images/thumbnail/bld-primary-web.avif'
 import img03 from '../assets/images/thumbnail/offers-primary-web.avif'
+import {Link} from 'react-router-dom'
+import monitorBanner from '../assets/images/thumbnail/monitor-banner.png'
+import products from '../data/products';
+import ProductCard from '../components/UI/ProductCard';
 
 const Cart = () => {
   return (
-    <div className='cart-index'>
+   <>
+     <section className='cart-index'>
         <div className="container">
             <div className="row">
                 <div className="cart-base">
@@ -17,7 +22,91 @@ const Cart = () => {
                 </div>
             </div>
         </div>
-    </div>
+    </section>
+    <section className="card-thumbnail">
+       <div className="container">
+        <div className="row">
+            <div className="card-index">
+            <div className="card-item">
+                <div className="card-thumbnail">
+                    <img src={img01} alt="Cart Image" />
+                </div>
+                <div className="card-body">
+                    <h3>Shop Prebuilts</h3>
+                    <Link>
+                    Shop Prebuilt PCs
+                    </Link>
+                </div>
+            </div>
+            <div className="card-item">
+                <div className="card-thumbnail">
+                    <img src={img02} alt="Cart Image" />
+                </div>
+                <div className="card-body">
+                    <h3>Shop Prebuilts</h3>
+                    <Link>
+                    Shop Prebuilt PCs
+                    </Link>
+                </div>
+            </div>
+            <div className="card-item">
+                <div className="card-thumbnail">
+                    <img src={img03} alt="Cart Image" />
+                </div>
+                <div className="card-body">
+                    <h3>Shop Prebuilts</h3>
+                    <Link>
+                    Shop Prebuilt PCs
+                    </Link>
+                </div>
+            </div>
+            </div>
+        </div>
+       </div>
+    </section>
+      <section className="main-content-grid">
+        <div className="container">
+          <div className="row">
+            <div className="full-main">
+              <div className="main-content">
+                <p>New</p>
+                <h3>Pay Later with Affirm</h3>
+                <p>
+                  Buy now, pay over time. Quick and easy. No hidden fees. Select
+                  "Affirm monthly payments" during checkout.
+                </p>
+                <Button variant="outlined" className='shop-btn'>Shop QHD Monitors</Button>
+              </div>
+              <div className="image-block">
+                <picture>
+                  <source media="(min-width: 768px)" srcset={monitorBanner} />
+                  <source
+                    media="(max-width: 768px)"
+                    srcset={monitorBanner}
+                  />
+                  <img
+                    className="background-img"
+                    src={monitorBanner}
+                    alt="Main Image"
+                  />
+                </picture>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+      <section className="shop-bestseller">
+        <div className="container">
+          <div className="row">
+            <div className="shop-index">
+              {products.slice(0,8).map((item,index) => (
+                <ProductCard item={item} key={index} />            
+              ))}
+            </div>
+          </div>
+        </div>
+      </section>
+   </>
   )
 }
 
