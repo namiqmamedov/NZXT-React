8b67f2cb53a236a6ebc39c70c3e88b829c305a19
namiqmamedov <namigmm@code.edu.az>
2023-07-04 16:51:35 +0400

    Created login page in responsive

---
+++ src/pages/Login.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import '../styles/login.css'
+
+const Login = () => {
+  return (
+    <div className='login-index'>
+      <svg  className='nzxt-logo' viewBox="0 0 94 24" width="1em" height="1em">
+        <path d="M6.907 0l8.227 11.906-.008-.201V0h6.944v24h-6.44L6.944 11.363V24H0V0h6.907zM94 0v6.322h-6.314v17.677h-7.374V6.322H74V.001h20zM56.406 0l3.902 6.16L64.466 0h7.623l-7.753 11.883 7.885 12.116h-8.082l-3.937-6.1-3.875 6.1h-7.894l7.735-11.896L48.32.001h8.086zM46.02 0l-9.49 17.914h9.29v6.085H25.743l9.319-17.952h-8.805V0h19.764z" fill-rule="evenodd">
+        </path>
+      </svg>
+      <div className="login-wrapper">
+        <div className="login-google">
+            <button>
+                <svg viewBox="0 0 48 48" width="1em" height="1em" ><path fill="#fbc02d" d="M44 20H24v8h11a12 12 0 11-3-13l6-6A20 20 0 004 24a20 20 0 1040-4z"></path><path fill="#e53935" d="M6 15l7 5a12 12 0 0119-5l6-6a20 20 0 00-32 6z"></path><path fill="#4caf50" d="M24 44c5 0 10-2 13-5l-6-5a12 12 0 01-18-6l-7 5c4 7 10 11 18 11z"></path><path fill="#1565c0" d="M44 20H24v8h11c0 2-2 4-4 6l6 5s7-5 7-15v-4z"></path></svg>
+                <span>Sign in with Google</span>
+            </button>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Login
--- src/routes/Routers.jsx
+++ src/routes/Routers.jsx
@@ -4,6 +4,7 @@ import Home from '../pages/Home'
 import Product from '../pages/Product'
 import ProductDetails from '../pages/ProductDetails'
 import Cart from '../pages/Cart'
+import Login from '../pages/Login'
 
 const Routers = () => {
   return (
@@ -11,9 +12,9 @@ const Routers = () => {
         <Route path='/' element= { <Navigate to='/home'/>} />
         <Route path='/home' element={<Home/>}/>
         <Route path='/product' element={<Product/>}/>
-        {/* <Route path='/product/:id' element={<ProductDetails/>}/> */}
         <Route path='/product-detail' element={<ProductDetails/>}/>
         <Route path='/cart' element={<Cart/>}/>
+        <Route path='/login' element={<Login/>}/>
     </Routes>
   )
 }
