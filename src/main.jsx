d7b818f141f1f4182bcf19d0b331a635c5748efa
namiqmamedov <namigmm@code.edu.az>
2023-07-11 13:50:56 +0400

    Created cart slice

--- src/main.jsx
+++ src/main.jsx
@@ -1,16 +1,22 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import {BrowserRouter as Router} from 'react-router-dom'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css'
+ 
+import store from './store/store.js';
+import { Provider } from 'react-redux';
+
 import ScrollToTop from './components/UI/ScrollToTop/ScrollToTop.jsx';
+import {BrowserRouter as Router} from 'react-router-dom'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Router>
     <ScrollToTop/>
+    <Provider store={store}>
       <App />
+    </Provider>
     </Router>
   </React.StrictMode>,
 )
